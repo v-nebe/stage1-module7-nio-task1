7adfb5b3ce578dbf8b4f7c3bf194199258f2804c
v-nebe <katya.shauneva@gmail.com>
2023-06-28 13:05:44 +0300

    tasks completed

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,39 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.nio.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file) throws IOException {
+        Path filePath = Paths.get(file.getPath());
+        BufferedReader reader = Files.newBufferedReader(filePath);
+
+        String name = null;
+        int age = 0;
+        String email = null;
+        Long phone = null;
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.startsWith("Name:")) {
+                name = line.substring(line.indexOf(":") + 1).trim();
+            } else if (line.startsWith("Age:")) {
+                age = Integer.parseInt(line.substring(line.indexOf(":") + 1).trim());
+            } else if (line.startsWith("Email:")) {
+                email = line.substring(line.indexOf(":") + 1).trim();
+            } else if (line.startsWith("Phone:")) {
+                phone = Long.parseLong(line.substring(line.indexOf(":") + 1).trim());
+            }
+        }
+
+        reader.close();
+        return new Profile(name, age, email, phone);
     }
 }
+
